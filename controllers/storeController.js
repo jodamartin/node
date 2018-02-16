8b76f2cb675e9d809d0c8776dba2f01cb747d85d
John M <john.jdm@zoho.com>
2018-02-16 12:30:53 -0500

    after 10

---
+++ controllers/storeController.js
@@ -0,0 +1,13 @@
+exports.homePage = (req, res) => {
+  console.log(req.name);
+  res.render('index');
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Add Store' });
+};
+
+exports.createStore = (req, res) => {
+  debugger;
+  res.json(req.body);
+};
